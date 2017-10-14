f97347df63d70aec167f8e07566f67004f715870
karan khatri <khatri.karankhatri.karan@gmail.com>
2017-10-15 00:40:38 +0530

    Add project page

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        pageTitle: 'Projects'
+
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unale to handle request.'
